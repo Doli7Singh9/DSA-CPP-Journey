d4ed3190d9517e6e198d05a243f21465c110de3c
Doli Singh <doliclout1234@gmail.com>
2026-01-22 06:10:06 +0530

    Time: 2 ms (57.77%), Space: 19.8 MB (18.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,7 +1,43 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<vector<int>> bucket(nums.size()+1);
+        unordered_map<int,int> freq;  // 4 4  5 6 6 6
+        for(int num: nums){
+            freq[num]++;   //  4- 2 and 5 - 1 and 6 - 3
+        }
+        
+        for(auto it: freq){
+            bucket[it.second].push_back(it.first);   //now we are filling these buckets 
+        }  // bucket(2 - freq) - 4(element)
+        // bucket(1 - freq) - 5(element)
+        // bucket(3 - freq) - 6(element)
+//final bucket 
+/*index → elements
+-----------------
+0 → []
+1 → [5]
+2 → [4]
+3 → [6]
+4 → []
+5 → []
+6 → []   
+ 
+*/
+        vector<int> ans;
+        for(int i = bucket.size() - 1; i >= 0 && ans.size() < k; i--) {
+            for(int num : bucket[i]) {
+                ans.push_back(num);
+                if(ans.size() == k)
+                    return ans;
+            }
+        }
+        return ans;
+    }  
+};
 
+        
+/* HASH MAP METHOD
         unordered_map<int , int> freq;
 
         for(int num : nums ){
@@ -24,3 +60,6 @@ public:
        return ans; 
     }
 };
+*/
+
+       
