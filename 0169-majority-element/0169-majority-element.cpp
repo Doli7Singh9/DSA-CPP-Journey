61d6928c2dc8296cd86c87627e2f266bc1a6555f
Doli Singh <doliclout1234@gmail.com>
2026-01-22 15:49:32 +0530

    Time: 0 ms (100%), Space: 28.2 MB (27.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority = 0;
-        int count = 0;
+        int majority = 0;  //the value that is currently “alive”
+        int count = 0;  //how many of it are alive after deleting pairs
 
-        for(int num : nums){
+        for(int num : nums){ //We scan the array once, left to right.
 
         if (count == 0){
             majority = num;
